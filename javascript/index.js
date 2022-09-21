9e6b32dc7e48e394aae51f44e865f9f3ec7ccb0b
DhWalidkhl <sm.walid69@yahoo.com>
2022-09-21 10:59:24 +0600

    Js updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,105 +1,38 @@
-document.getElementById('messi-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
+// add Player to the selected player div
+
+function addPlayer(name, button){
+    const playerList = document.getElementById('selected-players');
     const li = document.createElement('li');
-    li.innerText = 'Messi';
-    selectedPlayer.appendChild(li)
-
-    const btnDisable = document.getElementById('messi-btn');
-    btnDisable.setAttribute('disabled', true);
-})
-
-
-document.getElementById('ronaldo-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
-    const li = document.createElement('li');
-    li.innerText = 'Ronaldo';
-    selectedPlayer.appendChild(li)
-
-    const btnDisable = document.getElementById('ronaldo-btn');
-    btnDisable.setAttribute('disabled', true);
-})
-
-document.getElementById('neymar-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
-    const li = document.createElement('li');
-    li.innerText = 'Neymar';
-    selectedPlayer.appendChild(li);
-    const btnDisable = document.getElementById('neymar-btn');
-    btnDisable.setAttribute('disabled', true);
-})
-
-document.getElementById('messi2-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
-    const li = document.createElement('li');
-    li.innerText = 'Messi-2';
-    selectedPlayer.appendChild(li);
-    
-
-    const btnDisable = document.getElementById('messi2-btn');
-    btnDisable.setAttribute('disabled', true);
-
-})
+    li.classList.add('playerSelected')
+    li.innerHTML = name;
+    const playerListLeng = document.querySelectorAll(".playerSelected");
+    if(playerListLeng.length < 5){
+        playerList.appendChild(li);
+    }
+    else{
+        alert("You Can not Add more than 5 Player");
+        return;
+    }
+    button.setAttribute('disabled', true);
+}
 
 
-document.getElementById('ronaldo2-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
-    const li = document.createElement('li');
-    li.innerText = 'Ronaldo-2';
-    selectedPlayer.appendChild(li)
 
-    const btnDisable = document.getElementById('ronaldo2-btn');
-    btnDisable.setAttribute('disabled', true);
-})
+// Selected Player Cost
 
-document.getElementById('neymar2-btn').addEventListener('click', function(){
-    const selectedPlayer = document.getElementById('selected-players');
-    const li = document.createElement("li");
-    li.innerText = 'Neymar-2';
-    selectedPlayer.appendChild(li)
-
-    const btnDisable = document.getElementById('neymar2-btn');
-    btnDisable.setAttribute('disabled', true);
-})
-
-
-document.getElementById('player-cost-btn').addEventListener('click', function(){
-    const playerPrice = document.getElementById('player-price');
-    const playerCostString = playerPrice.value;
-    const playerCost = parseFloat(playerCostString);
-    const selectedPlayer = document.querySelectorAll('.player-selected li');
-    const totalSelectedString = selectedPlayer.length;
-    
-    const totalSelected = parseInt(totalSelectedString);
-    const totalCost = playerCost * totalSelected;
+function playerCost(){
+    const playerPriceString = document.getElementById('player-price').value;
     const playerExpenseCostString = document.getElementById('player-expense');
-    const playerExpenseCost = playerExpenseCostString.innerText;
-    const playerExpense = parseFloat(playerExpenseCost);
-    playerExpenseCostString.innerText = totalCost;
-})
+    playerExpenseCostString.innerText = parseFloat(playerPriceString) * document.querySelectorAll(".playerSelected").length;
+}
 
 
-
-document.getElementById('total-cost').addEventListener('click', function(){
-    const managerCostValue = document.getElementById('manager-cost');
-    const managerCostString = managerCostValue.value;
-    const managerCost = parseFloat(managerCostString);
-
-    const coachCostValue = document.getElementById('coach-cost');
-    const coachCostString = coachCostValue.value;
-    const coachCost = parseFloat(coachCostString);
-    
+// calculate total cost
+function totalCost(){
+    const playerExpenseCostString = document.getElementById('player-expense').innerText;
+    const coachPriceString = document.getElementById('coach-cost').value || 0;
+    const managerPriceString = document.getElementById('manager-cost').value || 0;
     const inTotal = document.getElementById('total-amount');
-    const inTotalString = inTotal.innerText;
-    const inTotalCost = parseFloat(inTotalString);
+    inTotal.innerText = parseFloat(coachPriceString) + parseFloat(managerPriceString) + parseFloat(playerExpenseCostString);
+}
 
-    const playerExpenseCostString = document.getElementById('player-expense');
-    const playerExpenseCost = playerExpenseCostString.innerText;
-    const playerCost = parseFloat(playerExpenseCost);
-      
-    const inTotalAmountString = managerCost + coachCost + playerCost;
-    const inTotalAmount = parseFloat(inTotalAmountString);
-
-
-    inTotal.innerText = inTotalAmount;
-    
-})
